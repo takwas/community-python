4e753bec85cc4ffef03e3601db281328bc4dcf72
theobouwman <theobouwman98@gmail.com>
2017-01-03 21:44:14 +0100

    tests

---
+++ test.py
@@ -0,0 +1,15 @@
+import unittest
+from app import app
+
+
+class FlaskTestCase(unittest.TestCase):
+
+    # Ensure that Flask was set up correctly
+    def test_index(self):
+        tester = app.test_client(self)
+        response = tester.get('/auth/login', content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+
+
+if __name__ == '__main__':
+    unittest.main()
