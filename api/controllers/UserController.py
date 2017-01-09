e67ab741fb7bb0b1bd4623f307ab405c40d2e619
theobouwman <theobouwman98@gmail.com>
2017-01-09 17:43:17 +0100

    current_user usercontroller

--- api/controllers/UserController.py
+++ api/controllers/UserController.py
@@ -1,8 +1,10 @@
+from flask import request
 from ..blueprint import api
 from middleware import token_verification
 
 
-@api.route('/me')
+@api.route('/user/me')
 @token_verification
 def me():
-    return 'me'
+    current_user = request.current_user['user']
+    return current_user
