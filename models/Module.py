7ce0e99d5eb0e185e772c4639114e411c8123293
theobouwman <theobouwman98@gmail.com>
2017-01-17 15:01:17 +0100

    fix

--- models/Module.py
+++ models/Module.py
@@ -1,9 +1,9 @@
-from peewee import PrimaryKeyField, ForeignKeyField, DateField
+from peewee import PrimaryKeyField, ForeignKeyField, DateTimeField
 from models import BaseModel, Module_Type
-import time
+import datetime
 
 
 class Module(BaseModel):
     id = PrimaryKeyField()
     type = ForeignKeyField(Module_Type, related_name='module_type_id')
-    build_on = DateField(default=time.strftime('%Y/%m/%d'))
+    build_on = DateTimeField(default=datetime.datetime.now())
