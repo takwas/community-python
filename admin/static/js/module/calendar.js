426b22369ba88f4f0f7ae89df13e5494b84822aa
theobouwman <theobouwman98@gmail.com>
2017-01-31 15:43:03 +0100

    calendar minDate | location

--- admin/static/js/module/calendar.js
+++ admin/static/js/module/calendar.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
     $('#module_calendar').fullCalendar({
         header: {
             left: 'prev, next today',
-            center: 'title',
             right: 'month'
         },
         events: '/admin/api/modules/module/' + $('#module_calendar').data('module-uuid') +'/locations'
