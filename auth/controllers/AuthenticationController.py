157b288f2df929b1b63603e003b4c1d3551dbbff
theobouwman <theobouwman98@gmail.com>
2017-02-01 18:25:23 +0100

    auth switch role

--- auth/controllers/AuthenticationController.py
+++ auth/controllers/AuthenticationController.py
@@ -1,12 +1,13 @@
 from ..blueprint import auth
-from models import User
+from models import User, User_Role, Role
 from flask import redirect, url_for, request, make_response, jsonify, flash, current_app, session, render_template
 from playhouse.shortcuts import model_to_dict
 import datetime
 import jwt
 import bcrypt
 from middleware import AuthMiddleware
-from helpers.validate import uuid_validation
+from helpers import validate
+from enums.auth import AuthRoleTypes
 from ..forms.register import SimpleLoginForm
 
 
@@ -47,8 +48,10 @@ def authenticate():
 @auth.route('/login', methods=['GET', 'POST'])
 @AuthMiddleware.no_login_required
 def login():
+    form = SimpleLoginForm(request.form)
+
     # login post method
-    if request.method == 'POST':
+    if request.method == 'POST' and form.validate():
         email = request.form['email']
         password = request.form['password']
 
@@ -67,40 +70,74 @@ def login():
 
         user = user.get()
 
-        # check us user has admin role
-        access = False
-        for assigned_role in user.assigned_roles:
-            if assigned_role.role.role == 'admin':
-                access = True
-
-        # check admin role
-        if access is False:
-            flash('U bent geen admin')
-            return redirect(url_for('auth.login'))
-
         # check password
         right_password = bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8'))
         if not right_password:
             flash('Foutive inloggegevens')
             return redirect(url_for('auth.login'))
 
+        # check us user has admin role
+        assigned_roles = user.assigned_roles
+        if len(assigned_roles) > 0:
+            access = True
+        else:
+            access = False
+
+        # check admin role
+        if access is False:
+            flash('U kunt niet inloggen')
+            return redirect(url_for('auth.login'))
+
         # add user to session
         session['user'] = model_to_dict(user)  # convert to dict
-        session['user_language'] = request.headers.get('Accept-Language')
-        flash('succesvol ingelogd')
-        return redirect(url_for('admin.index'))
+
+        # to role switching page
+        return redirect(url_for('auth.switch_role'))
 
     # login page
     else:
-        form = SimpleLoginForm(request.form)
         return render_template('pages/login.html', form=form)
+
+
+@auth.route('/switch-role', methods=['GET', 'POST'])
+@AuthMiddleware.login_required
+def switch_role():
+    u = User.from_oject(session['user'])
+    roles = u.assigned_roles
+    if request.method == 'POST':
+        role_value = request.form['role']
+        if not role_value:
+            flash('Kiez een rol')
+            return redirect(url_for('auth.switch_role'))
+
+        role = Role.select().where(Role.id == role_value)
+        if not role.exists():
+            flash('Rol bestaat niet')
+            return redirect(url_for('auth.switch_role'))
+
+        role = role.get()
+
+        for r in roles:
+            if role == r.role:
+                session['active_role'] = model_to_dict(role)
+                if role.role == AuthRoleTypes.ADMIN.value:
+                    return redirect(url_for('admin.index'))
+                elif role.role == AuthRoleTypes.CLIENT.value:
+                    return redirect('/client/')
+                else:
+                    return redirect('/')
+
+        flash('U heeft de gekozen rol niet')
+        return redirect(url_for('auth.switch_role'))
+
+    return render_template('pages/switch_role.html', roles=roles)
 
 
 @auth.route('/activate/<user_uuid>/<activation_key>')
 @AuthMiddleware.no_login_required
 def activate(user_uuid, activation_key):
     # check for valid uuid
-    if not uuid_validation(user_uuid):
+    if not validate.uuid_validation(user_uuid):
         flash('Ongeldige gegevens')
         return redirect(url_for('auth.login'))
 
@@ -118,6 +155,10 @@ def activate(user_uuid, activation_key):
 
     user.activated = True
     user.save()
+
+    role, created = Role.get_or_create(role='client')
+
+    new_role = User_Role.create(user=user, role=role)
 
     flash('Account geactiveerd')
     return redirect(url_for('auth.login'))
