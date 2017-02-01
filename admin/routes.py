316e4f96ac974f4332b99fbd48c779aa66929abf
theobouwman <theobouwman98@gmail.com>
2017-02-01 18:21:29 +0100

    middleware name

--- admin/routes.py
+++ admin/routes.py
@@ -1,11 +1,11 @@
 from models import initialize_db, close_db_connection
-from middleware import login_required
+from middleware import AuthMiddleware
 from .blueprint import admin
 from .controllers import IndexController, UserController, GroupController, ContractController, ModuleController, LocationController, ApiController
 
 
 @admin.before_request
-@login_required
+@AuthMiddleware.admin_role_required
 def before_request():
     initialize_db()
 
