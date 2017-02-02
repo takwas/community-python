cf28fb1e7037c2a335ab0d2d12a0c8fddeb0a796
theobouwman <theobouwman98@gmail.com>
2017-02-02 20:37:56 +0100

    tests

--- test.py
+++ test.py
@@ -10,15 +10,15 @@ class FlaskTestCase(unittest.TestCase):
         response = tester.get('/auth/login', content_type='html/text')
         self.assertEqual(response.status_code, 200)
 
-    def test_register_test_user(self):
-        tester = app.test_client(self)
-        response = tester.get('/auth/register/test', content_type='html/text')
-        self.assertEqual(response.status_code, 200)
-
-    def test_jwt_authentication(self):
-        tester = app.test_client(self)
-        response = tester.get('/auth/authenticate-jwt?email=theobouwman98@gmail.com&password=test', content_type='html/text')
-        self.assertEqual(response.status_code, 200)
+    # def test_register_test_user(self):
+    #     tester = app.test_client(self)
+    #     response = tester.get('/auth/register/test', content_type='html/text')
+    #     self.assertEqual(response.status_code, 200)
+    #
+    # def test_jwt_authentication(self):
+    #     tester = app.test_client(self)
+    #     response = tester.get('/auth/authenticate-jwt?email=theobouwman98@gmail.com&password=test', content_type='html/text')
+    #     self.assertEqual(response.status_code, 200)
 
 
 if __name__ == '__main__':
